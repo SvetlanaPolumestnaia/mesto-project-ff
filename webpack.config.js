22c46d966bbf039e93a7b05cfc09830a31bde804
apolumestny <anpolumestny@gmail.com>
2023-11-07 19:30:04 +0300

    Update webpack and babel.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
-const path = require('path'); // подключаем path к конфигу вебпак
-const HtmlWebpackPlugin = require('html-webpack-plugin'); // подключите плагин 
-const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // подключили плагин 
-// подключите к проекту mini-css-extract-plugin
+const path = require('path'); 
+const HtmlWebpackPlugin = require('html-webpack-plugin'); 
+const { CleanWebpackPlugin } = require('clean-webpack-plugin'); 
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
@@ -11,36 +10,27 @@ module.exports = {
     filename: 'main.js',
         publicPath: ''
   },
-  mode: 'development', // добавили режим разработчика
+  mode: 'development', 
   devServer: {
-    static: path.resolve(__dirname, './dist'), // путь, куда "смотрит" режим разработчика
-    compress: true, // это ускорит загрузку в режиме разработки
-    port: 8080, // порт, чтобы открывать сайт по адресу localhost:8080, но можно поменять порт
+    static: path.resolve(__dirname, './dist'), 
+    compress: true, 
+    port: 8080, 
 
-    open: true // сайт будет открываться сам при запуске npm run dev
+    open: true 
   },
   module: {
-    rules: [ // rules — это массив правил
-      // добавим в него объект правил для бабеля
+    rules: [
       {
-        // регулярное выражение, которое ищет все js файлы
         test: /\.js$/,
-        // при обработке этих файлов нужно использовать babel-loader
         use: 'babel-loader',
-        // исключает папку node_modules, файлы в ней обрабатывать не нужно
         exclude: '/node_modules/'
       },
-      // добавили правило для обработки файлов
       {
-            // регулярное выражение, которое ищет все файлы с такими расширениями
         test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
         type: 'asset/resource'
       },
       {
-        // применять это правило только к CSS-файлам
         test: /\.css$/,
-        // при обработке этих файлов нужно использовать
-        // MiniCssExtractPlugin.loader и css-loader
         use: [MiniCssExtractPlugin.loader, {
           loader: 'css-loader'
         }]
@@ -52,6 +42,6 @@ module.exports = {
         template: './src/index.html'
       }),
     new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin() // подключение плагина для объединения файлов
+    new MiniCssExtractPlugin()
   ]
 }
